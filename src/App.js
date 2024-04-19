3e784c1761ae1a46e26bc4c591e9393c112d0566
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-19 15:02:14 -0400

    Refactor App component to use react-router-dom's useRoutes hook for rendering routes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { useRoutes } from 'react-router-dom';
+import routes from './components/Routes';
 
 function App() {
+
+  const Router = useRoutes(routes)
+
   return (
     <div className="App">
-      
+      {Router}
     </div>
   );
 }
