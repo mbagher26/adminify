1df6a9c667fb10be4a5db598ef74a741385d6730
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-23 07:27:47 -0400

    Add title and description to Feature component

--- src/components/features/Feature.js
+++ src/components/features/Feature.js
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import { Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
 
 const Feature = () => {
           return (
                     <div className='feature'>
                               <Box className='featurecontent'>
-                                        {/* Feature content will be added here */}
+                                        <Typography sx={{ fontSize: 20 }} color="rgb(76, 62, 75)">
+                                                  Sales
+                                        </Typography>
+                                        <Box className='featurecontainer'>
+                                                  {/* Feature container content will be added here */}
+                                        </Box>
+                                        <Typography variant="body2" color='rgb(144, 148, 148)'>
+                                                  well meaning and kindly.
+                                        </Typography>
                               </Box>
                     </div>
           )
