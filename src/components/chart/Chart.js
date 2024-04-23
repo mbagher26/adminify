f7a1e93d15d72940d64d69c56d200868d04d55d0
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-23 12:20:00 -0400

    add className chart & delete style in LineChart tag & update stroke

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,25 +1,16 @@
 import React from 'react'
 import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import './Chart.css'
 
-const Chart = ({data , dataKey}) => {
+const Chart = ({ data, dataKey }) => {
           return (
-                    <div>
-                              <ResponsiveContainer width="100%" height="100%">
-                                        <LineChart
-                                                  width={500}
-                                                  height={300}
-                                                  data={data}
-                                                  margin={{
-                                                            top: 5,
-                                                            right: 30,
-                                                            left: 20,
-                                                            bottom: 5,
-                                                  }}
-                                        >
-                                                  <CartesianGrid strokeDasharray="3 3" />
-                                                  <XAxis dataKey="name" />
+                    <div className='chart'>
+                              <ResponsiveContainer width="100%" height="100%" aspect={4}>
+                                        <LineChart data={data}>
+                                                  <CartesianGrid strokeDasharray="3 3"  stroke='#e0dfdf'/>
+                                                  <XAxis dataKey="name" stroke='#5550bd'/>
                                                   <Tooltip />
-                                                  <Line type="monotone" dataKey={dataKey} stroke="#82ca9d" />
+                                                  <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
                                         </LineChart>
                               </ResponsiveContainer>
                     </div>
