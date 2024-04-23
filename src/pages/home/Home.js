ce45b7af98c905aab1f45df673bdda91afbfbf11
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-23 12:26:58 -0400

    Update Home component to use MUI Grid layout and pass data to Chart

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,14 +2,27 @@ import React from 'react'
 import Feature from '../../components/features/Feature'
 import './Home.css'
 import Chart from '../../components/chart/Chart'
+import { xAxisData } from '../../data'
+import Grid from '@mui/material/Grid';
 
 const Home = () => {
   return (
     <div className='home'>
-      <Feature />
-      <Feature />
-      <Feature />
-      <Chart/>
+      <Grid container  className='feature-grid'>
+        <Grid  >
+          <Feature />
+        </Grid>
+        <Grid>
+          <Feature />
+        </Grid>
+        <Grid>
+          <Feature />
+        </Grid>
+      </Grid>
+
+      <Grid>
+        <Chart data={xAxisData} dataKey="Sale" />
+      </Grid>
     </div>
   )
 }
