f761b1d8578d159481a7b3c1836a7fad948b4b5e
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-23 07:24:45 -0400

    Here's your code snippet with the unnecessary imports removed:

--- src/components/features/Feature.js
+++ src/components/features/Feature.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
-
+import { Box } from '@mui/material';
 
 const Feature = () => {
           return (
                     <div className='feature'>
-                          
+                              <Box className='featurecontent'>
+                                        {/* Feature content will be added here */}
+                              </Box>
                     </div>
           )
 }
