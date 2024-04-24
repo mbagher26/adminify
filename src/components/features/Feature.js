da3dbcf0f600f5d9d5ed6e3cac5fc37dc2922046
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-24 14:43:45 -0400

    Rendered feature content dynamically for each data item in featureData.

--- src/components/features/Feature.js
+++ src/components/features/Feature.js
@@ -10,50 +10,52 @@ import './Feature.css'
 const Feature = () => {
 
           const [featureData, setFeatureData] = useState(null);
-          
+
           useEffect(() => {
                     const fetchsupabase = async () => {
                               const { data, error } = await supabase
                                         .from("feature")
                                         .select()
-                                        if (error) {
-                                                  console.log(error);
-                                                  setFeatureData(null)
-                                        }
-                                        if (data) {
-                                                  setFeatureData(data)
-                                                  console.log(data);
-                                        }
+                              if (error) {
+                                        console.log(error);
+                                        setFeatureData(null)
                               }
-                              
-                              fetchsupabase()
-                    }, [])
-                    
+                              if (data) {
+                                        setFeatureData(data)
+                                        console.log(data);
+                              }
+                    }
+
+                    fetchsupabase()
+          }, [])
+
 
           return (
                     <div className='feature'>
-                              <Box className='featurecontent'>
-                                        <Typography sx={{ fontSize: 20 }} color="rgb(76, 62, 75)">
-                                                  Sales
-                                        </Typography>
-                                        <Box className='featurecontainer'>
-                                                  <Stack className='feature-item'>
-                                                            <AttachMoneyIcon fontSize='large' />
-                                                            <Typography color="dark" variant='h4'>
-                                                                      2.415
-                                                            </Typography>
-                                                  </Stack>
-                                                  <Stack className='feature-item'>
-                                                            <Typography color="rgb(85, 55, 62)" >
-                                                                      -11.4
-                                                            </Typography>
-                                                            <ArrowDownwardIcon color='error' />
-                                                  </Stack>
+                              {featureData && featureData.map(data => (
+                                        <Box className='featurecontent'>
+                                                  <Typography sx={{ fontSize: 20 }} color="rgb(76, 62, 75)">
+                                                            {data.title}
+                                                  </Typography>
+                                                  <Box className='featurecontainer'>
+                                                            <Stack className='feature-item'>
+                                                                      <AttachMoneyIcon fontSize='large' />
+                                                                      <Typography color="dark" variant='h4'>
+                                                                                {data.price}
+                                                                      </Typography>
+                                                            </Stack>
+                                                            <Stack className='feature-item'>
+                                                                      <Typography color="rgb(85, 55, 62)" >
+                                                                                {data.change}
+                                                                      </Typography>
+                                                                      <ArrowDownwardIcon color='error' />
+                                                            </Stack>
+                                                  </Box>
+                                                  <Typography variant="body2" color='rgb(144, 148, 148)'>
+                                                            {data.desc}
+                                                  </Typography>
                                         </Box>
-                                        <Typography variant="body2" color='rgb(144, 148, 148)'>
-                                                  well meaning and kindly.
-                                        </Typography>
-                              </Box>
+                              ))}
                     </div>
           )
 }
