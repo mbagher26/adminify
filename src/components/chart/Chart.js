d678b3ca456d2aaf4a68cd43c8f5dac5328e0854
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-24 09:35:22 -0400

    - Updated the Chart component to accept additional props: 'title' and 'grid'.

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import './Chart.css'
 
-const Chart = ({ data, dataKey }) => {
+const Chart = ({ data, dataKey, title, grid }) => {
           return (
                     <div className='chart'>
+                              <h3 className='chart-title'>{ title }</h3>
                               <ResponsiveContainer width="100%" height="100%" aspect={4}>
                                         <LineChart data={data}>
-                                                  <CartesianGrid strokeDasharray="3 3"  stroke='#e0dfdf'/>
+                                                  { grid && <CartesianGrid strokeDasharray="3 3"  stroke='#e0dfdf'/>}
                                                   <XAxis dataKey="name" stroke='#5550bd'/>
                                                   <Tooltip />
                                                   <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
