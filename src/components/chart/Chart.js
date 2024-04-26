bb849e51c8e0e14f1dd8349d2feac32185978fb7
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-26 09:20:07 -0400

    delete height in ResponsiveContainer

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -5,8 +5,8 @@ import './Chart.css'
 const Chart = ({ data, dataKey, title, grid }) => {
           return (
                     <div className='chart'>
+                              <ResponsiveContainer width="100%" aspect={4}>
                               <h3 className='chart-title'>{ title }</h3>
-                              <ResponsiveContainer width="100%" height="100%" aspect={4}>
                                         <LineChart data={data}>
                                                   { grid && <CartesianGrid strokeDasharray="3 3"  stroke='#e0dfdf'/>}
                                                   <XAxis dataKey="name" stroke='#5550bd'/>
