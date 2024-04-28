823c8c53130427036632c5c5cdc51cf1cd37ba04
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-28 12:27:32 -0400

    import widgets component and use Grid to home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,17 +2,28 @@ import React from 'react'
 import Feature from '../../components/features/Feature'
 import './Home.css'
 import Chart from '../../components/chart/Chart'
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import WidgetSm from '../../components/widgetsm/WidgetSm';
+import WidgetLg from '../../components/widgetlg/WidgetLg';
+
 
 
 const Home = () => {
 
 
-  
+
   return (
     <Box className='home'>
-        <Feature />
-        <Chart grid/>
+      <Feature />
+      <Chart grid />
+      <Grid container spacing={2}>
+        <Grid md={4} item>
+          <WidgetSm />
+        </Grid>
+        <Grid md={8} item>
+          <WidgetLg />
+        </Grid>
+      </Grid>
     </Box>
   )
 }
