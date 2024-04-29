893d992c0bcfe4c7e3188e108d38e5f725dead7c
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-29 13:12:55 -0400

    add tabel to dom

--- src/pages/users/UserList.js
+++ src/pages/users/UserList.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+
 
 const UserList = () => {
   return (
-    <div>UserList</div>
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+      />
+    </div>
   )
 }
 
