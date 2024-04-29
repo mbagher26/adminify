780764dbac203092e329e8d57414380b56e89a5a
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-29 10:24:16 -0400

    fetchData from supabase and dynamic dom

--- src/components/widgetsm/WidgetSm.js
+++ src/components/widgetsm/WidgetSm.js
@@ -2,26 +2,46 @@ import * as React from 'react';
 import { List, ListItem, ListItemText, Avatar, ListItemAvatar, Typography } from '@mui/material';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import './WidgetSm.css'
+import { useState, useEffect } from 'react';
+import supabase from '../../SupabaseClient';
 
 const WidgetSm = () => {
+          const [widgetData, setWidgetData] = useState();
+
+          useEffect(() => {
+                    const fetchData = async () => {
+                              const { data, erroe } = await supabase
+                                        .from("newmembers")
+                                        .select()
+                              if (erroe) {
+                                        console.log(erroe);
+                              }
+                              if (data) {
+                                        setWidgetData(data)
+                              }
+                    }
+                    fetchData()
+          }, []);
           return (
                     <div className='widgetsm'>
                               <List dense sx={{ width: '100%' }}>
 
-                                        <Typography sx={{padding: '10px'}}>erdyctfuvygbhnj</Typography>
-                                        <ListItem>
-                                                  <ListItemAvatar>
-                                                            <Avatar
-                                                                      alt="Avatar"
-                                                                      src='/Image/avatar.jpg'
-                                                            />
-                                                  </ListItemAvatar>
-                                                  <ListItemText className='list-item-text' primary="Line item" secondary="Line item" />
+                                        <Typography sx={{ padding: '10px' }}>New Join Members</Typography>
+                                        {widgetData && widgetData.map(data => (
+                                                  <ListItem key={data.id}>
+                                                            <ListItemAvatar>
+                                                                      <Avatar
+                                                                                alt="Avatar"
+                                                                                src={data.img}
+                                                                      />
+                                                            </ListItemAvatar>
+                                                            <ListItemText className='list-item-text' primary={data.username} secondary={data.title} />
 
-                                                  <Avatar className='widgetsm-avatar'>
-                                                            <VisibilityIcon />
-                                                  </Avatar>
-                                        </ListItem>
+                                                            <Avatar className='widgetsm-avatar'>
+                                                                      <VisibilityIcon />
+                                                            </Avatar>
+                                                  </ListItem>
+                                        ))}
                               </List>
                     </div>
           )
