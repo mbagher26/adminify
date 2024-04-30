0548f3c2e17aeed45cb076b07a3c6a06ba216d0a
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-04-30 11:32:22 -0400

    fetch rowData and dynamic dom

--- src/pages/users/UserList.js
+++ src/pages/users/UserList.js
@@ -1,20 +1,58 @@
 import React from 'react'
 import { DataGrid } from '@mui/x-data-grid';
-
+import { useState, useEffect } from 'react';
+import supabase from '../../SupabaseClient';
+import './UserList.css'
+import { Box } from '@mui/material';
 
 const UserList = () => {
+
+  const [rowData, setRowData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from("users")
+        .select()
+      if (error) {
+        console.log(error);
+      }
+      if (data) {
+        setRowData(data)
+      }
+    }
+    fetchData()
+  }, []);
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    {
+      field: 'username',
+      headerName: 'User',
+      width: 250,
+    },
+    { field: 'email', headerName: 'Email', width: 150 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 120,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction',
+      width: 120,
+    },
+  ];
+
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-      />
-    </div>
+    <Box className='userlist' >
+      {rowData &&
+        <DataGrid
+          rows={rowData}
+          columns={columns}
+        />}
+    </Box>
   )
 }
 
