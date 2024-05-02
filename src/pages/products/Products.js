0fd2c043c6a0c6b1c74878831378d384d05f7bd8
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-05-02 09:38:47 -0400

    past Userlist component to Product page

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,9 +1,113 @@
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+import { useState, useEffect } from 'react';
+import supabase from '../../SupabaseClient';
+import { Box, Avatar } from '@mui/material';
+import { Link } from 'react-router-dom';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
-const Products = () => {
+const UserList = () => {
+
+  const [rowData, setRowData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from("users")
+        .select()
+      if (error) {
+        console.log(error);
+      }
+      if (data) {
+        setRowData(data)
+      }
+    }
+    fetchData()
+  }, []);
+
+  const deleteHandler = async (id) => {
+
+    const { data, error } = await supabase
+      .from("users")
+      .delete()
+      .eq('id', id)
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+
+
+    const newrowData = rowData.filter(data => {
+      return data.id !== id
+    })
+
+    setRowData(newrowData)
+
+
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 80 },
+    {
+      field: 'username',
+      headerName: 'User',
+      width: 260,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link className='user-link' to='/'>
+              <Avatar src={params.row.avatar} />
+              {params.row.username}
+            </Link>
+          </>
+        );
+      }
+    },
+    { field: 'email', headerName: 'Email', width: 160 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 130,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction',
+      width: 130,
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 130,
+      renderCell: (params) => {
+        return (
+          <div className='user-action'>
+            <button>Edit</button>
+
+            <DeleteOutlineOutlinedIcon onClick={() => deleteHandler(params.row.id)} />
+
+          </div>
+        )
+      }
+    }
+  ];
+
+
   return (
-    <div>Products</div>
+    <Box className='userlist' >
+      {rowData &&
+        <DataGrid
+          rows={rowData}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 4 },
+            },
+          }}
+        />}
+    </Box>
   )
 }
 
-export default Products
+export default UserList
