b08588e9e4630f81252bce535ca1ac7578b8b151
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-05-03 11:32:33 -0400

    Added a Create button to the Product component title bar.

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,8 +1,17 @@
+import { Box, Stack, Typography, Button } from '@mui/material'
 import React from 'react'
+import './Product.css'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <Box className='product'>
+      <Stack direction='row' className="product-title-container">
+        <Typography className="producttitle">Product</Typography>
+        <Button variant="contained" className='btn-create'>
+          Create
+        </Button>
+      </Stack>
+    </Box>
   )
 }
 
