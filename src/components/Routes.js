ee8b0a0b185aaae8d72fb31aa3e00e863e696291
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-05-03 13:59:00 -0400

    add route for Product component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,6 @@
 import Home from "../pages/home/Home";
 import NewUser from "../pages/newuser/NewUser";
+import Product from "../pages/product/Product";
 import Products from "../pages/products/Products";
 import UserList from "../pages/users/UserList";
 
@@ -8,7 +9,8 @@ const routes = [
           { path: '/home', element: <Home /> },
           { path: '/users', element: <UserList /> },
           { path: '/newuser', element: <NewUser /> },
-          { path: '/products', element: <Products/>}
+          { path: '/products', element: <Products /> },
+          { path: '/products/:productID', element: <Product/>}
 ]
 
 export default routes
