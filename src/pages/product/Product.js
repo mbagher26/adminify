26293822a8c7ec3123b8b1d90b0b7110557c9e1f
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-05-03 11:44:01 -0400

    Create product information layout using Stack component

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography, Button } from '@mui/material'
+import { Box, Stack, Typography, Button, Avatar } from '@mui/material'
 import Chart from './../../components/chart/Chart'
 import React from 'react'
 import './Product.css'
@@ -16,6 +16,42 @@ const Product = () => {
       < Stack className='product-chart' >
         <Chart grid title="Sale In Month" data={productData} dataKey="sales"/>
       </Stack >
+
+
+      <Stack className='product-info'>
+        <Stack className='product-info-top'>
+          <Avatar src='/Image/dell.jpg' />
+          <Typography className='product-name'>Dell</Typography>
+        </Stack>
+        <Stack className='product-info-button'>
+
+          <Stack className='product-info-item'>
+            <Stack className='product-key'>ID</Stack>
+            <Stack className='product-value'>1</Stack>
+          </Stack>
+
+          <Stack className='product-info-item'>
+            <Stack className='product-key'>Name</Stack>
+            <Stack className='product-value'>Dell</Stack>
+          </Stack>
+
+          <Stack className='product-info-item'>
+            <Stack className='product-key'>Sales</Stack>
+            <Stack className='product-value'>$90000</Stack>
+          </Stack>
+
+          <Stack className='product-info-item'>
+            <Stack className='product-key'>Active</Stack>
+            <Stack className='product-value'>yes</Stack>
+          </Stack>
+
+          <Stack className='product-info-item'>
+            <Stack className='product-key'>In Stock</Stack>
+            <Stack className='product-value'>no</Stack>
+          </Stack>
+        </Stack>
+
+      </Stack>
     </Box>
   )
 }
