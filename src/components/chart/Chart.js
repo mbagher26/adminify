ce7da9731bd7c2b2c8336d081a33b68871e388db
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-05-03 07:53:16 -0400

    Refactor Chart component to accept props for data, dataKey, and title

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -3,19 +3,19 @@ import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } f
 import './Chart.css'
 
 
-const Chart = ({ grid }) => {
+const Chart = ({ grid, data, dataKey, title }) => {
 
       
 
           return (
                     <div className='chart'>
                               <ResponsiveContainer width="100%" aspect={4}>
-                                        <h3 className='chart-title'>Month Sale</h3>
-                                        <LineChart data={xAxisData} className='line-chart'>
+                                        <h3 className='chart-title'>{title}</h3>
+                                        <LineChart data={data} className='line-chart'>
                                                   { grid && <CartesianGrid strokeDasharray="3 3"  stroke='#e0dfdf'/>}
                                                   <XAxis dataKey="name" stroke='#5550bd'/>
                                                   <Tooltip />
-                                                  <Line type="monotone" dataKey="sale" stroke="#5550bd" />
+                                                  <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
                                         </LineChart>
                               </ResponsiveContainer>
                     </div>
