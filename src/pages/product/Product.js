0876a5cadef3b719543f7fcda250ce083eee94a8
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-05-05 10:25:30 -0400

    Refactor Product component: Changed the grid layout and adjusted styles for better organization and UI presentation.

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,4 +1,4 @@
-import { Stack, Typography, Button, Avatar, Grid } from '@mui/material'
+import { Box, Stack, Typography, Button, Avatar, Grid } from '@mui/material'
 import { useState, useEffect } from 'react'
 import Chart from './../../components/chart/Chart'
 import React from 'react'
@@ -26,53 +26,64 @@ const Product = () => {
   }, []);
 
   return (
-    <Grid container className='product'>
-      <Grid md={12} item direction='row' className="product-title-container">
-        <Typography className="producttitle" variant='h5'>Product</Typography>
-        <Button variant="contained" className='btn-create'>
-          Create
-        </Button>
+    <Box className='product'>
+      <Grid container={true} spacing={6}>
+
+        <Grid md={12} item >
+          <Stack direction='row' className="product-title-container">
+            <Typography className="product-title" variant='h5'>Product</Typography>
+            <Button variant="contained" className='btn-create'>
+              Create
+            </Button>
+          </Stack>
+        </Grid>
+
+        < Grid md={6} item >
+          <Stack className='product-chart'>
+            <Chart grid title="Sale In Month" data={productData} dataKey="sales" />
+          </Stack>
+        </Grid >
+
+
+        <Grid md={6} item >
+          <Stack direction='row' className='product-info'>
+
+            <Stack className='product-info-left'>
+              <Avatar src='/Image/dell.jpg' variant='square' sx={{ width: '250px', height: '150px', margin: '10px' }} />
+              <Typography className='product-name'>Dell</Typography>
+            </Stack>
+
+            <Stack className='product-info-right'>
+
+              <Stack className='product-info-item'>
+                <Typography className='product-key'>ID</Typography>
+                <Typography className='product-value'>1</Typography>
+              </Stack>
+
+              <Stack className='product-info-item'>
+                <Typography className='product-key'>Name</Typography>
+                <Typography className='product-value'>Dell</Typography>
+              </Stack>
+
+              <Stack className='product-info-item'>
+                <Typography className='product-key'>Sales</Typography>
+                <Typography className='product-value'>$90000</Typography>
+              </Stack>
+
+              <Stack className='product-info-item'>
+                <Typography className='product-key'>Active</Typography>
+                <Typography className='product-value'>yes</Typography>
+              </Stack>
+
+              <Stack className='product-info-item'>
+                <Typography className='product-key'>Is Stock</Typography>
+                <Typography className='product-value'>no</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Grid>
       </Grid>
-
-      < Grid md={6} item className='product-chart' >
-        <Chart grid title="Sale In Month" data={productData} dataKey="sales" />
-      </Grid >
-
-
-      <Grid md={6} item className='product-info'>
-        <Stack className='product-info-top'>
-          <Avatar src='/Image/dell.jpg' variant='square' sx={{ width: '160px', height: '80px', margin: '10px' }} />
-          <Typography className='product-name'>Dell</Typography>
-        </Stack>
-        <Stack className='product-info-button'>
-
-          <Stack className='product-info-item'>
-            <Typography className='product-key'>ID</Typography>
-            <Typography className='product-value'>1</Typography>
-          </Stack>
-
-          <Stack className='product-info-item'>
-            <Typography className='product-key'>Name</Typography>
-            <Typography className='product-value'>Dell</Typography>
-          </Stack>
-
-          <Stack className='product-info-item'>
-            <Typography className='product-key'>Sales</Typography>
-            <Typography className='product-value'>$90000</Typography>
-          </Stack>
-
-          <Stack className='product-info-item'>
-            <Typography className='product-key'>Active</Typography>
-            <Typography className='product-value'>yes</Typography>
-          </Stack>
-
-          <Stack className='product-info-item'>
-            <Typography className='product-key'>Is Stock</Typography>
-            <Typography className='product-value'>no</Typography>
-          </Stack>
-        </Stack>
-      </Grid>
-    </Grid>
+    </Box>
   )
 }
 
